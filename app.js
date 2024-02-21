416add746c73f579af2d26da6bdd7c6c6f451075
MartinPettersen <searchingtheskyfor@gmail.com>
2024-02-21 09:18:57 +0100

    remove left over code

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const byListe = [
 ];
 
 const getCityWeatherData = async (cityLocation) => {
-  console.log(":::: " + cityLocation);
   const latlongCity = "lat=59.9333&lon=10.7166";
   const url = `https://api.met.no/weatherapi/locationforecast/2.0/compact?${cityLocation}`;
 
@@ -23,9 +22,7 @@ const getCityWeatherData = async (cityLocation) => {
   };
   try {
     const data = await fetch(url, options);
-    // console.log(data)
     const weatherData = await data.json();
-    // console.log(weatherData);
 
     return weatherData;
   } catch (error) {
@@ -54,115 +51,295 @@ const readline = require("node:readline").createInterface({
   output: process.stdout,
 });
 
-/*
-
-readline.question(`Hvilken by ønsker du å vite om? `, (byNavn) => {
-  
-    console.log(`Henter informasjon om ${byNavn}`);
-  const reply = finnesBy(byNavn);
-
-  const today = new Date();
-  const tomorrow = new Date(today);
-  tomorrow.setDate(today.getDate() + 1);
-
-  let isoFormatTomorrow = tomorrow.toISOString();
-
-  console.log(tomorrow);
-  if (reply.status) {
-    console.log(
-      `Temperator for ${byNavn}  ${isoFormatTomorrow.slice(
-        5,
-        7
-      )}.${isoFormatTomorrow.slice(8, 10)}.${isoFormatTomorrow.slice(0, 4)}:`
-    );
-    getCityWeatherData(reply.message)
-      .then((data) => {
-        console.log("");
-        data.properties.timeseries.map((dataBlock) => {
-          console.log(dataBlock.time)
-         if (isoFormatTomorrow.slice(0, 10) == dataBlock.time.slice(0, 10)) {
-            const tid = dataBlock.time.slice(11, 16);
-            const temperatur = dataBlock.data.instant.details.air_temperature;
-            // console.log(`Kl ${tid} ${temperatur} grader`);
-          }
-        });
-        // console.log(data.properties.timeseries[0].time);
-        // console.log(data.properties.timeseries[0].data.instant.details.air_temperature);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  } else {
-    console.log(reply.message);
-  }
-
-  readline.close();
-});
-*/
-
 const cityQuestion = () => {
   return new Promise((resolve, reject) => {
     readline.question(`Hvilken by ønsker du å vite om? `, (byNavn) => {
-      // console.log(byNavn)
       resolve(byNavn);
-      // return byNavn
     });
+  });
+};
+
+const variableQuestion = () => {
+  return new Promise((resolve, reject) => {
+    readline.question(
+      `Hva vil du vite? (regn, temperatur, vind) `,
+      (brukerVariabler) => {
+        resolve(brukerVariabler);
+      }
+    );
   });
 };
 
 
 
+const test = (data, isoFormatTomorrow, brukerVariabler) => {
+  let totalTemp = 0;
+  let count = 0;
+  let totalVind = 0;
+
+  
+  function Etappe(time,temp, low, high, count, regn, vind) {
+    this.time = time;    
+    this.temp = temp;
+    this.low = low;
+    this.high = high;
+    this.count = count;
+    this.regn = regn;
+    this.vind = vind;
+  }
+
+  //const morgen = new Etappe(0, "Talon TSi", 1993);
+
+  let morgen = 0;
+  let morgenLow = 9999;
+  let morgenHigh = -9999;
+  let morgencount = 0;
+  let morgenRegn = 0;
+  let morgenVind = 0;
+
+  let formiddag = 0;
+  let formiddagLow = 9999;
+  let formiddagHigh = -9999;
+  let formiddagcount = 0;
+  let formiddagRegn = 0;
+  let formiddagVind = 0;
+
+  let ettermiddag = 0;
+  let ettermiddagLow = 9999;
+  let ettermiddagHigh = -9999;
+  let ettermiddagcount = 0;
+  let ettermiddagRegn = 0;
+  let ettermiddagVind = 0;
+
+  let kveld = 0;
+  let kveldLow = 9999;
+  let kveldHigh = -9999;
+  let kveldcount = 0;
+  let kveldRegn = 0;
+  let kveldVind = 0;
+
+  data.properties.timeseries.map((dataBlock) => {
+    if (isoFormatTomorrow.slice(0, 10) == dataBlock.time.slice(0, 10)) {
+      const tid = dataBlock.time.slice(11, 13);
+
+      const temperatur = dataBlock.data.instant.details.air_temperature;
+      const vind = dataBlock.data.instant.details.wind_speed;
+      let regn = 0;
+
+      if (dataBlock.data.next_1_hours) {
+        regn = dataBlock.data.next_1_hours.details.precipitation_amount;
+      } else if (dataBlock.data.next_6_hours) {
+        regn = dataBlock.data.next_6_hours.details.precipitation_amount;
+      }
+
+      totalTemp += temperatur;
+      count += 1;
+      totalVind += vind;
+
+      if (tid < 8) {
+        morgen += temperatur;
+
+        morgenRegn += regn;
+        morgenVind += vind;
+
+        morgencount += 1;
+        if (temperatur < morgenLow) {
+          morgenLow = temperatur;
+        }
+        if (temperatur > morgenHigh) {
+          morgenHigh = temperatur;
+        }
+      } else if (tid < 12) {
+        formiddag += temperatur;
+        formiddagcount += 1;
+
+        formiddagRegn += regn;
+
+        formiddagVind += vind;
+
+        if (temperatur < formiddagLow) {
+          formiddagLow = temperatur;
+        }
+        if (temperatur > formiddagHigh) {
+          formiddagHigh = temperatur;
+        }
+      } else if (tid < 18) {
+        ettermiddag += temperatur;
+        ettermiddagcount += 1;
+
+        ettermiddagRegn += regn;
+
+        ettermiddagVind += vind;
+
+        if (temperatur < ettermiddagLow) {
+          ettermiddagLow = temperatur;
+        }
+        if (temperatur > ettermiddagHigh) {
+          ettermiddagHigh = temperatur;
+        }
+      } else {
+        kveld += temperatur;
+        kveldcount += 1;
+
+        kveldVind += vind;
+
+        kveldRegn += regn;
+
+        if (temperatur < kveldLow) {
+          kveldLow = temperatur;
+        }
+        if (temperatur > kveldHigh) {
+          kveldHigh = temperatur;
+        }
+      }
+    }
+  });
+  const uke = [
+    "Søndag",
+    "Mandag",
+    "Tirsdag",
+    "Onsdag",
+    "Torsdag",
+    "Fredag",
+    "Lørdag",
+  ];
+  const month = [
+    "Januar",
+    "Februar",
+    "Mars",
+    "April",
+    "Mai",
+    "Juni",
+    "Juli",
+    "August",
+    "September",
+    "Oktober",
+    "November",
+    "Desember",
+  ];
+
+  const date = new Date(isoFormatTomorrow);
+  console.log(
+    `${uke[date.getDay()]} ${isoFormatTomorrow.slice(8, 10)}.${
+      month[parseInt(isoFormatTomorrow.slice(5, 7))]
+    } ${isoFormatTomorrow.slice(0, 4)}  ${
+      brukerVariabler.includes("temperatur")
+        ? `(snittemperatur ${
+            Math.round((totalTemp / count) * 100) / 100
+          } grader)`
+        : ""
+    } : ${
+      brukerVariabler.includes("regn")
+        ? `${
+            Math.round(
+              (morgenRegn + formiddagRegn + ettermiddagRegn + kveldRegn) * 100
+            ) / 100
+          }mm regn`
+        : ""
+    } ${
+      brukerVariabler.includes("vind")
+        ? `snittvind ${totalVind / count} m/s`
+        : ""
+    }
+    \n`
+  );
+  console.log(
+    `00-08: ${
+      brukerVariabler.includes("temperatur")
+        ? ` fra ${morgenLow} til ${morgenHigh} grader (snittemperatur ${
+            Math.round((morgen / morgencount) * 100) / 100
+          } grader) `
+        : ""
+    } ${
+      brukerVariabler.includes("regn")
+        ? `${Math.round(morgenRegn * 100) / 100}mm regn`
+        : ""
+    } ${
+      brukerVariabler.includes("vind") ? `${morgenVind / morgencount} m/s` : ""
+    }
+    \n`
+  );
+  console.log(
+    `08-12: ${
+      brukerVariabler.includes("temperatur")
+        ? `fra ${formiddagLow} til ${formiddagHigh} grader (snittemperatur ${
+            Math.round((formiddag / formiddagcount) * 100) / 100
+          } grader) `
+        : ""
+    } ${
+      brukerVariabler.includes("regn")
+        ? `${Math.round(formiddagRegn * 100) / 100}mm regn`
+        : ""
+    } ${
+      brukerVariabler.includes("vind")
+        ? `${formiddagVind / formiddagcount} m/s`
+        : ""
+    }
+    \n`
+  );
+  console.log(
+    `12-18:  ${
+      brukerVariabler.includes("temperatur")
+        ? `fra ${ettermiddagLow} til ${ettermiddagHigh} grader (snittemperatur ${
+            Math.round((ettermiddag / ettermiddagcount) * 100) / 100
+          } grader) `
+        : ""
+    }${
+      brukerVariabler.includes("regn")
+        ? `${Math.round(ettermiddagRegn * 100) / 100}mm regn`
+        : ""
+    } ${
+      brukerVariabler.includes("vind")
+        ? `${ettermiddagVind / ettermiddagcount} m/s`
+        : ""
+    }
+    \n`
+  );
+  console.log(
+    `18-00: ${
+      brukerVariabler.includes("temperatur")
+        ? `fra ${kveldLow} til ${kveldHigh} grader (snittemperatur ${
+            Math.round((kveld / kveldcount) * 100) / 100
+          } grader)  `
+        : ""
+    }${
+      brukerVariabler.includes("regn")
+        ? `${Math.round(kveldRegn * 100) / 100}mm regn`
+        : ""
+    } ${brukerVariabler.includes("vind") ? `${kveldVind / kveldcount} m/s` : ""}
+    \n\n\n`
+  );
+};
+
 const main = async () => {
   let byNavn = await cityQuestion();
   let reply = finnesBy(byNavn);
-  while(reply.status == false){
+  while (reply.status == false) {
     console.log(reply.message);
     byNavn = await cityQuestion();
     reply = finnesBy(byNavn);
   }
 
-
-  // --------------
-
+  let brukerVariabler = await variableQuestion();
+  brukerVariabler = brukerVariabler.replace(/\s/g, "").split(",");
 
   const today = new Date();
   const tomorrow = new Date(today);
   tomorrow.setDate(today.getDate() + 1);
 
-  let isoFormatTomorrow = tomorrow.toISOString();
-
-  console.log(tomorrow);
   if (reply.status) {
-    console.log(
-      `Temperator for ${byNavn}  ${isoFormatTomorrow.slice(
-        5,
-        7
-      )}.${isoFormatTomorrow.slice(8, 10)}.${isoFormatTomorrow.slice(0, 4)}:`
-    );
-    getCityWeatherData(reply.message)
-      .then((data) => {
-        console.log("");
-        data.properties.timeseries.map((dataBlock) => {
-          // console.log(dataBlock.time)
-         if (isoFormatTomorrow.slice(0, 10) == dataBlock.time.slice(0, 10)) {
-            const tid = dataBlock.time.slice(11, 16);
-            const temperatur = dataBlock.data.instant.details.air_temperature;
-            console.log(`Kl ${tid} ${temperatur} grader`);
-          }
-        });
-        // console.log(data.properties.timeseries[0].time);
-        // console.log(data.properties.timeseries[0].data.instant.details.air_temperature);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    const weatherdata = await getCityWeatherData(reply.message);
+
+    for (i = 1; i < 8; i++) {
+      const nextDay = new Date(today);
+      nextDay.setDate(today.getDate() + i);
+      const isoFormatNextDay = nextDay.toISOString();
+      test(weatherdata, isoFormatNextDay, brukerVariabler);
+    }
   } else {
     console.log(reply.message);
   }
 
-  // -----------
-
-  console.log(reply);
   readline.close();
 };
 
